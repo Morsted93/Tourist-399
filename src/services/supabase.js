98d860290d163339c61bfd455939979149599054
Morsted93 <bujibdog@gmail.com>
2024-10-20 00:30:19 +0300

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,9 +2,9 @@ import { createClient } from '@supabase/supabase-js'
 
 // DB = tNPnWxdIfnvMI3jC
 
-const SUPABASE_URL = 'https://gtcqgcjnvurdbaqstryi.supabase.co'
+const SUPABASE_URL = 'https://bdwjylvdxxqzkjrfafur.supabase.co'
 const SUPABASE_API_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0Y3FnY2pudnVyZGJhcXN0cnlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzNzczNTksImV4cCI6MjA0Mzk1MzM1OX0.edcCD2797-oPnheJBCPK-GdiN3OM0SbNg5T21wL0Bqw'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkd2p5bHZkeHhxemtqcmZhZnVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkzNjk4NjgsImV4cCI6MjA0NDk0NTg2OH0.6yic3bXuQtPg8i3c0znc7lxgMhcc4bM7txCKuwBDfcQ'
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY)
 export default supabase
